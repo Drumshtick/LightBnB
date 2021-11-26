e856d505df3f16ba8627f31ddef5f1a6c2f88689
Drumshtick <ekodnar@outlook.com>
2021-11-26 18:16:00 +0000

    removed comments and debug code

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -93,7 +93,6 @@ const getAllReservations = function(guest_id, limit = 10) {
   LIMIT $2;
   `, [guest_id, limit])
   .then((result) => {
-    console.log(result.rows);
     return result.rows;
   })
   .catch((err) => {
